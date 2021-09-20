0b8897a032a4582116d271e38068f89209463f35
Vinay Verma <vinay.verma@zemosolabs.com>
2021-09-20 20:21:06 +0530

    feat: adding user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.create(user_params)
     if @user.save
@@ -10,6 +14,16 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'User was successfully updated.'
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
-  resources :users, only: %i[create]
+  resources :users, except: %i[new]
 end
