134ca190355b6c095c473f715223f1c6959aff60
Vinay Verma <vinay.verma@zemosolabs.com>
2021-09-20 23:05:38 +0530

    feat: adding user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def create
     @user = User.create(user_params)
     if @user.save
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def gravatar_for(user, option = { size: 80 })
+    size = option[:size]
+    gravatar_url = "https://about.me/cdn-cgi/image/q=40,dpr=2,f=auto,fit=cover,w=#{size},h=#{size},gravity=0.331x0.149/https://assets.about.me/background/users/v/i/n/vinay.an32_gmail.com_1464877189_87.jpg"
+    image_tag(gravatar_url, alt: user.username)
+  end
+
 end
